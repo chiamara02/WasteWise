3358a10b38483226b84e84daf1d829e50aba911a
Chiara <chiaramarangoni02@gmail.com>
2024-05-10 19:44:51 +0200

    aggiunti i metodi nuovaSegnalazione() e mostraSegnalazioni() per utente cittadino ed ente.

--- backend/users/Cittadino.js
+++ backend/users/Cittadino.js
@@ -1,10 +1,17 @@
 const LoggedUser = require("./LoggedUser");
+const GestoreSegnalazioni = require("../handlers/GestoreSegnalazioni")
 
 class Cittadino extends LoggedUser {
     constructor() {
         super();
     }
+
+    static async nuovaSegnalazione(utente, descrizione, zona, foto){
+        return GestoreSegnalazioni.nuovaSegnalazione(utente, descrizione, zona, foto);
+    }
     
 }
+
+
 
 module.exports = Cittadino;
