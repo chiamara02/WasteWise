3c913f9b2ea0b38fe16358ee36451464e2e2ddd5
bissochan-UNI <luca.bissoli@studenti.unitn.it>
2024-05-11 11:43:47 +0200

    aggiunto schema tasse per db

---
+++ backend/handlers/GestoreTasse.js
@@ -0,0 +1,5 @@
+const tasse = require('../db/pagamentoTasse').pagamentoTasse;
+const NotFoundException = require("../exceptions/NotFoundException");
+const FailedDependencyException = require("../exceptions/FailedDependencyException");
+const WrongPasswordException = require("../exceptions/WrongPasswordException");
+
