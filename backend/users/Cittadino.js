941eb54fe0abacb7ad91aa9ca70e3933dd33a3e2
bissochan-UNI <luca.bissoli@studenti.unitn.it>
2024-05-12 22:58:59 +0200

    fixed conflicts su api tasse unificate

--- backend/users/Cittadino.js
+++ backend/users/Cittadino.js
@@ -1,4 +1,9 @@
 const LoggedUser = require("./LoggedUser");
+<<<<<<< HEAD
+=======
+const Tasse = require('../db/tasse').tasse;
+const TaxHandler = require("../handlers/TaxHandler");
+>>>>>>> S1-M1
 const GestoreSegnalazioni = require("../handlers/GestoreSegnalazioni")
 
 class Cittadino extends LoggedUser {
@@ -6,8 +11,21 @@ class Cittadino extends LoggedUser {
         super();
     }
 
+<<<<<<< HEAD
     static async nuovaSegnalazione(utente, descrizione, zona, foto){
         return GestoreSegnalazioni.nuovaSegnalazione(utente, descrizione, zona, foto);
+=======
+    static async getAllTasse(idUser) {
+        return TaxHandler.getAllTasse(idUser);
+    }
+    
+    static async getPendingTasse(idUser) {
+        return TaxHandler.getTasseByStatus(idUser, "nonPagato");
+}
+
+    static async getPaidTasse(idUser){
+        return TaxHandler.getTasseByStatus(idUser, "pagato")
+>>>>>>> S1-M1
     }
     
 }
