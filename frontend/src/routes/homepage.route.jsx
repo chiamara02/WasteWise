07bcd51938542c0387677a50a0079fd2ff51b1f1
ValeMayora <vale.mayora@gmail.com>
2024-05-14 19:14:57 +0200

    HomePage, WipPage, immagini utili

--- frontend/src/routes/homepage.route.jsx
+++ frontend/src/routes/homepage.route.jsx
@@ -1,10 +1,21 @@
-import HelloWorld from "../components/helloWorld.component";
+import { useState } from 'react'
 
+import Navbar from '../components/navbar.component'
+import HeroSection from '../components/herosection.component'
+import Mission from '../components/mission.component'
+import ValuesSection from '../components/infofunzionalita.component'
+import Footer from '../components/footer.component'
+import InfoFunzionalita from '../components/infofunzionalita.component'
 
 export default function HomePage() {
+
     return (
         <>
-            <HelloWorld />
+            <Navbar />,
+            <HeroSection />,
+            <Mission />,
+            <InfoFunzionalita />,
+            <Footer />
         </>
     )
 }
